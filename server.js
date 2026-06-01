0917c3fee036b653ade7caeba886c9372873db6c
achrafalalei1239-langop <achrafalalei1239@gmail.com>
2026-06-01 16:16:06 +0100

    Remove unused PermissionFlagsBits import

--- server.js
+++ server.js
@@ -2,7 +2,7 @@ const express = require('express');
 const session = require('express-session');
 const fs = require('fs');
 const path = require('path');
-const { Client, GatewayIntentBits, ChannelType, PermissionFlagsBits } = require('discord.js');
+const { Client, GatewayIntentBits, ChannelType } = require('discord.js');
 
 const app = express();
 const PORT = process.env.PORT || 3000;
